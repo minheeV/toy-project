ba4c6755a9e41cbb2df1b083a8f3dffdc2251fd6
minhee <minhee0123v@gmail.com>
2023-02-15 17:03:04 +0900

    Feat : Naver Map compose로 띄우기
    https://github.com/fornewid/naver-map-compose 사용하여 구현

--- compose/toy-compose/app/src/main/java/com/toy/compose_retrofit/MainActivity.kt
+++ compose/toy-compose/app/src/main/java/com/toy/compose_retrofit/MainActivity.kt
@@ -6,15 +6,26 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.map.NaverMapSdk.NaverCloudPlatformClient
+import com.naver.maps.map.compose.ExperimentalNaverMapApi
+import com.naver.maps.map.compose.NaverMap
 import com.toy.compose_retrofit.ui.theme.Compose_retrofitTheme
 
+
+@ExperimentalNaverMapApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        //AndroidManifest에 CLIENT_ID 등록했지만 지도가 나타나지 않아 우선 API로 CLIENT_ID 등록
+        NaverMapSdk.getInstance(this).client =
+            NaverCloudPlatformClient(BuildConfig.NAVER_CLIENT_ID)
+
         setContent {
             Compose_retrofitTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,14 +34,19 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
     }
 }
 
+@ExperimentalNaverMapApi
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    NaverMap(
+        modifier = Modifier.fillMaxSize()
+    )
 }
 
+@ExperimentalNaverMapApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
