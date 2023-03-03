f964f5cab90ba15091ee6ccb1e084aaf229dd24b
minhee <minhee0123v@gmail.com>
2023-03-03 14:52:06 +0900

    코드 정리

--- compose/toy-compose/app/src/main/java/com/toy/compose_retrofit/MainActivity.kt
+++ compose/toy-compose/app/src/main/java/com/toy/compose_retrofit/MainActivity.kt
@@ -2,40 +2,26 @@
 
 package com.toy.compose_retrofit
 
-import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.toy.compose_retrofit.ui.theme.Compose_retrofitTheme
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-
 import com.naver.maps.map.compose.*
 
 
 
 class MainActivity : ComponentActivity(){
-
-    companion object {
-        const val LOCATION_PERMISSION_REQUEST_CODE = 1000
-    }
-
     private val permissionRequest = 99
     private var permissions = arrayOf(
         android.Manifest.permission.ACCESS_FINE_LOCATION,
@@ -73,67 +59,24 @@ class MainActivity : ComponentActivity(){
 @SuppressLint("MissingPermission")
 @Composable
 fun DrawMap() {
-    NaverMap(
-        locationSource = rememberFusedLocationSource(),
-        properties = MapProperties(
-            locationTrackingMode = LocationTrackingMode.Follow,
-        ),
-        uiSettings = MapUiSettings(
-            isLocationButtonEnabled = true,
-        )
-    )
-/*
     val context = LocalContext.current as Activity
-    val locationSources = remember{
-        FusedLocationSource(context, LOCATION_PERMISSION_REQUEST_CODE)
-    }
-    var testPos = LatLng(37.584697, 126.885875)
-
-    val cameraPositionState: CameraPositionState = rememberCameraPositionState {
-        position = CameraPosition(testPos, 13.0)
-    }
-
-
-    val mapProperties by remember {
-        mutableStateOf(
-            MapProperties(
-                maxZoom = 15.0,
-                minZoom = 8.0,
-            )
-        )
-    }
-
-    val mapUiSettings by remember {
-        mutableStateOf(
-            MapUiSettings(isLocationButtonEnabled = true)
-        )
-    }
-
 
     Box(Modifier.fillMaxSize()) {
         NaverMap(
             locationSource = rememberFusedLocationSource(),
             properties = MapProperties(
-                locationTrackingMode = LocationTrackingMode.Face,
-            ),//mapProperties,
+                locationTrackingMode = LocationTrackingMode.Follow,
+            ),
             uiSettings = MapUiSettings(
                 isLocationButtonEnabled = true,
             ),
-            cameraPositionState = cameraPositionState,
             onLocationChange = {
-                println("lon : ${it.longitude} , lat : ${it.latitude}")
+                //println("lon : ${it.longitude} , lat : ${it.latitude}")
             }
 
-        ){
-            Marker(
-                state = MarkerState(position = testPos),
-                captionText = "Marker",
-                captionColor = Color.Blue
-            )
-        }
+        ){}
     }
 
-*/
 
 }
 
